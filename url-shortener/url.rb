efbb328aedcbc8d007a2dfed1ee965831c98809b
Russell Jennings <violentpurr@gmail.com>
2018-01-12 01:48:41 -0500

    adds sample code for url-shortener

--- url-shortener/url.rb
+++ url-shortener/url.rb
@@ -1,11 +1,28 @@
+# == Schema Information
+#
+# Table name: urls
+#
+#  id                :integer          not null, primary key
+#  path              :string
+#  short_code         :string
+#  visit_count       :integer          default(0)
+#  admin_disabled    :boolean          default(FALSE)
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# add_index :urls, :short_code, unique: true
+
 class Url < ApplicationRecord
 
-  #
-  # TODO Replace this comment with a description of the database schema
-  #
+  validates :path, :format => URI::regexp(%w(http https))
 
-  #
-  # TODO Add validations that support the described schema
-  #
+  before_create :create_short_code
+
+  def create_short_code
+    self.short_code = SecureRandom.urlsafe_base64(4)
+    until self.class.where(short_code: self.short_code).empty?
+      self.short_url = SecureRandom.urlsafe_base64(4)
+    end
+  end
 
 end
--- url-shortener/url_controller.rb
+++ url-shortener/url_controller.rb
@@ -1,9 +1,40 @@
 class UrlController < ApplicationController
 
-  def show
+  # https://lend.to/xYzb
+  # find the related URL and redirect to it
+  def visit
+    url = Url.where(short_code: params[:short_code]).first
+    if url
+      View.create(short_code: url.short_code)
+      redirect_to url.path
+    else
+      render :status => 404
+    end
   end
 
+  # https://lendevor.com/url-shortner/xYzb
+  # show the URL shortened address and view counts
+  def show
+    @url = Url.where(short_code: params[:short_code]).first
+  end
+
+  def new
+  end
+
+  # https://lendevor.com/url-shortner/
+  # create the shortened URL
   def create
+    if url = Url.create(url_params)
+      redirect_to url_path(url)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:path)
   end
 
 end
